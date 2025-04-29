21c7faff2c1ba1ecdf14156d8f34e23584654f52
Timon Dubreuil <timondubreuil@outlook.com>
2025-04-29 19:13:09 +0200

    added Tarot card

---
+++ TheStar.cpp
@@ -0,0 +1,11 @@
+
+std::string TheStar::getTitle()
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+int TheStar::getIndex()
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
---
+++ TheStar.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "TarotCard.h"
+
+class TheStar : public TarotCard
+{
+	std::string getDescription() override
+	{
+		return "- My Amy, what is this, some kind of hotline ? - All entities take 10x damage, weapons always drop loaded from enemies. You cannot store weapons in the inventory. You cannot reload weapons.";
+	}
+
+
+	std::string getTitle() override
+	{
+		return "The Star";
+	}
+
+
+	int getIndex() override
+	{
+		return 17;
+	}
+
+};
