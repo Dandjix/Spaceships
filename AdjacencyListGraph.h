6ba78cafd8f91e42ca88e26a8c704c709c4a1d88
Timon Dubreuil <timondubreuil@outlook.com>
2025-04-30 23:57:37 +0200

    Made AdjacencyListGraph be defined in the header so that it is unit testable

--- AdjacencyListGraph.h
+++ AdjacencyListGraph.h
@@ -14,23 +14,80 @@ protected:
 	std::unordered_map<T, std::list<std::pair<T,int>>> adjacencyListMap;
 
 public:
-	void addVertex(T vertex);
-	void removeVertex(T vertex);
-	const bool hasVertex(T vertex);
+	void addVertex(T vertex)
+	{
+		adjacencyListMap[vertex] = std::list<std::pair<T, int>>();
+	}
+	void removeVertex(T vertex)
+	{
+		adjacencyListMap.erase(vertex);
+	}
+	const bool hasVertex(T vertex)
+	{
+		return adjacencyListMap.find(vertex) != adjacencyListMap.end();
+	}
 
-	void addEdge(T vertex1, T vertex2, int distance);
-	void removeEdge(T vertex1, T vertex2);
-	const bool hasEdge(T vertex1, T vertex2);
+	void addEdge(T vertex1, T vertex2, int distance)
+	{
+		auto& adj_list_1 = adjacencyListMap[vertex1];
+		adj_list_1.push_back({ vertex2, distance });
 
-	const std:: list<T> connected(T vertex);
+		auto& adj_list_2 = adjacencyListMap[vertex2];
+		adj_list_2.push_back({ vertex1, distance });
+	}
+	void removeEdge(T vertex1, T vertex2)
+	{
+		auto& adj_list_1 = adjacencyListMap[vertex1];
+		adj_list_1.remove_if([&](const std::pair<T, int>& edge) {
+			return edge.first == vertex2;
+			});
 
-	const int pixelDistance(T vertex1, T vertex2);
-	const int graphDistance(T vertex1, T vertex2);
-	/// <summary>
-	/// this implements A*
-	/// </summary>
-	/// <param name="from"></param>
-	/// <param name="to"></param>
-	/// <returns></returns>
-	const std::list<T> shortestPath(T from, T to);
+		auto& adj_list_2 = adjacencyListMap[vertex2];
+		adj_list_2.remove_if([&](const std::pair<T, int>& edge) {
+			return edge.first == vertex1;
+			});
+
+	}
+	const bool hasEdge(T vertex1, T vertex2)
+	{
+		auto& adj_list = adjacencyListMap[vertex1];
+		for (auto pair : adj_list)
+		{
+			if (pair.first == vertex2)
+				return true;
+		}
+		return false;
+	}
+
+	const std:: list<T> connected(T vertex)
+	{
+		std::list<std::pair<T, int>>& it = adjacencyListMap[vertex];
+		std::list<T> adjacentEdges;
+		for (const std::pair<T, int>& pair : it)
+		{
+			adjacentEdges.push_back(pair.first);
+		}
+		return adjacentEdges;
+	}
+
+	//const int pixelDistance(T vertex1, T vertex2)
+	//{
+	//	return -1;
+	//}
+	//const int graphDistance(T vertex1, T vertex2)
+	//{
+	//	return -1;
+	//}
+	///// <summary>
+	///// this implements Djikstra's algorithm
+	///// </summary>
+	///// <param name="from"></param>
+	///// <param name="to"></param>
+	///// <returns></returns>
+	//const std::list<T> shortestPath(T from, T to)
+	//{
+	//	return std::list<T>();
+	//}
 };
+
+extern template class AdjacencyListGraph<int>;
