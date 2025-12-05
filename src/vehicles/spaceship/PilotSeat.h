73d02d926afea4c7fe55d79f0c6fe95ca5fc96ad
Timon <timondubreuil@outlook.com>
2025-12-05 13:30:24 +0100

    Revert "Pilot seat doesnt move"
    This reverts commit d2a27e8fa252d59965a0b2ccc2c571994f50eb91.

--- src/vehicles/spaceship/PilotSeat.h
+++ src/vehicles/spaceship/PilotSeat.h
@@ -33,6 +33,4 @@ public:
         }
         return new PilotSeat(Vector2Int::fromJson(json["position"]),json["angle"]);
     }
-
-    void movePosition(Vector2Float delta, SpaceShip *space_ship) override{}
 };
