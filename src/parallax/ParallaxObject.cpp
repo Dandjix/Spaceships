6fb51ab1978cbc0674cd355ba851d1f05fc74b11
Timon <timondubreuil@outlook.com>
2025-12-12 23:55:32 +0100

    parallax fixed fr fr og

--- src/parallax/ParallaxObject.cpp
+++ src/parallax/ParallaxObject.cpp
@@ -25,52 +25,60 @@ float /**
  * @param  screen dimensions
  * @return the distance of the camera relative to the entity plane, a positive number
  */
-getCameraDistance(float field_of_view, float screenWidth)
-{
-    float A = (screenWidth * static_cast<float>(Vectors::getFactor())) /2.0f;
+getCameraDistance(float field_of_view, float screenWidth) {
+    float A = (screenWidth * static_cast<float>(Vectors::getFactor())) / 2.0f;
 
-    float T = std::tan(field_of_view/2);
+    float T = std::tan(field_of_view / 2);
 
-    float O = A*T;
+    float O = A * T;
 
     return O;
 }
 
-void ParallaxObject::render(SDL_Renderer* renderer, const ExteriorRenderingContext& context, SpaceShip * ship) const {
+void ParallaxObject::render(SDL_Renderer *renderer, const ExteriorRenderingContext &context, SpaceShip *ship) const {
+    auto cameraDistance = getCameraDistance(
+        120.0f,
+        static_cast<float>(context.camera_info.screenDimensions.x) * context.camera_info.cameraScale
+    );
 
-        auto cameraDistance = getCameraDistance(
-            120.0f,
-            static_cast<float>(context.camera_info.screenDimensions.x)*context.camera_info.cameraScale
-        );
-
-        float parallax_factor = cameraDistance / (cameraDistance + depth);
-        // Clamp to avoid weirdness
-        parallax_factor = std::clamp(parallax_factor, 0.0f, 1.0f);
-
-
-        Vector2Float foreground_position = context.camera_info.worldToScreenPoint(position);
-
-        Vector2Float screen_center = Vectors::toVector2Float(context.camera_info.screenDimensions) / 2;
-
-        Vector2Float parallax_position = Vector2Float::lerp(foreground_position,screen_center,1-parallax_factor);
-
-        float texture_w, texture_h;
-        SDL_GetTextureSize(texture, &texture_w, &texture_h);
-
-        // texture_w /= context.camera_info.cameraScale;
-        // texture_h /= context.camera_info.cameraScale;
+    float parallax_factor;
+    if (depth == 0) {
+        parallax_factor = 0;
+    }
+    else {
+        parallax_factor = depth / (cameraDistance + depth);
+    }
+    // Clamp to avoid weirdness
+    parallax_factor = std::clamp(parallax_factor, 0.0f, 1.0f);
 
 
-        SDL_FRect destRect = {
-            parallax_position.x - texture_w * 0.5f,
-            parallax_position.y - texture_h * 0.5f,
-            texture_w,
-            texture_h
-        };
+    Vector2Float foreground_position = context.camera_info.worldToScreenPoint(position);
 
-        SDL_RenderTextureRotated(renderer, texture, nullptr, &destRect,
-            context.camera_info.getScreenObjectAngle(angle), nullptr, SDL_FLIP_NONE);
+    Vector2Float screen_center = Vectors::toVector2Float(context.camera_info.screenDimensions) / 2;
 
+    Vector2Float parallax_position = Vector2Float::lerp(foreground_position, screen_center, parallax_factor);
+
+    float texture_w, texture_h;
+    SDL_GetTextureSize(texture, &texture_w, &texture_h);
+
+    texture_w *= 1-parallax_factor;
+    texture_h *= 1-parallax_factor;
+
+    texture_w /= context.camera_info.cameraScale;
+    texture_h /= context.camera_info.cameraScale;
+
+    texture_w*= sizeMultiplier;
+    texture_h*= sizeMultiplier;
+
+    SDL_FRect destRect = {
+        parallax_position.x - texture_w * 0.5f,
+        parallax_position.y - texture_h * 0.5f,
+        texture_w,
+        texture_h
+    };
+
+    SDL_RenderTextureRotated(renderer, texture, nullptr, &destRect,
+                             context.camera_info.getScreenObjectAngle(angle), nullptr, SDL_FLIP_NONE);
 }
 
 //
